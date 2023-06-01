7e1a241aeeafbb06f87f27857c5378138dca9192
Andres Eduardo Quezada Bendaña <andresequez@gmail.com>
2023-05-31 23:45:44 -0600

    Add Start

--- quesoPortafolio/src/components/Start/Start.jsx
+++ quesoPortafolio/src/components/Start/Start.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './Start.module.css';
 
 function Start() {
+  useEffect(() => {
+    window.addEventListener('load', () => {
+      window.scrollTo(0, 200); // Cambia el valor "500" según tus necesidades
+    });
+  }, []);
+
   return (
     <div className={styles.track}>
       <div className={styles.message}>
         <div className={styles.title}>
-          Hello! My name is Andres Quezada
+          Hello! Go to your start position
+        </div>
+        <div className={styles.subtitle}>
+          Use the arrow keys to move
         </div>
       </div>
     </div>
