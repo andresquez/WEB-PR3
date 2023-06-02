5ea359166c5e3fc21f48b962ca69721c3754cec5
Andres Eduardo Quezada Bendaña <andresequez@gmail.com>
2023-06-02 01:28:22 -0600

    Add fireworks map

--- quesoPortafolio/src/components/FinishLine/FinishLine.jsx
+++ quesoPortafolio/src/components/FinishLine/FinishLine.jsx
@@ -3,22 +3,21 @@ import styles from './FinishLine.module.css';
 import fireworks from '../../../public/fireworks.gif';
 
 function FinishLine() {
+  const fireworkClasses = Array.from({ length: 14 }, (_, index) => `fireworks${index + 1}`);
+
   return (
     <div className={styles.track}>
-      <img src={fireworks} alt="fireworks" className={styles.fireworks} />
-      <img src={fireworks} alt="fireworks" className={styles.fireworks2} />
-      <img src={fireworks} alt="fireworks" className={styles.fireworks3} />
-      <img src={fireworks} alt="fireworks" className={styles.fireworks4} />
-      <img src={fireworks} alt="fireworks" className={styles.fireworks5} />
-      <img src={fireworks} alt="fireworks" className={styles.fireworks6} />
-      <img src={fireworks} alt="fireworks" className={styles.fireworks7} />
-      <img src={fireworks} alt="fireworks" className={styles.fireworks8} />
-      <img src={fireworks} alt="fireworks" className={styles.fireworks9} />
-      <img src={fireworks} alt="fireworks" className={styles.fireworks10} />
-      <img src={fireworks} alt="fireworks" className={styles.fireworks11} />
-      <img src={fireworks} alt="fireworks" className={styles.fireworks12} />
-      <img src={fireworks} alt="fireworks" className={styles.fireworks13} />
-      <img src={fireworks} alt="fireworks" className={styles.fireworks14} />
+      {fireworkClasses.map((fireworkClass, index) => {
+        const uniqueKey = `firework-${index}`;
+        return (
+          <img
+            key={uniqueKey}
+            src={fireworks}
+            alt="fireworks"
+            className={`${styles[fireworkClass]} ${styles.fireworks}`}
+          />
+        );
+      })}
       <div className={styles.message}>
         <div className={styles.title}>
           Hello!
